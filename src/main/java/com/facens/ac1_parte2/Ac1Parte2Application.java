120ae806c7dba51aea3960384f4042809ecadbcb
ThomasJanoski <thomassilveira2020@gmail.com>
2025-09-24 19:12:31 -0300

    First Commit

---
+++ src/main/java/com/facens/ac1_parte2/Ac1Parte2Application.java
@@ -0,0 +1,13 @@
+package com.facens.ac1_parte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac1Parte2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac1Parte2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/facens/ac1_parte2/Ac1Parte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facens.ac1_parte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac1Parte2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
